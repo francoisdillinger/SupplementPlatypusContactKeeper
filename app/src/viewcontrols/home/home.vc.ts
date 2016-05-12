27dd1ae1b4d8c244abcd884ed4dd68f8cab380f5
James Jester <elmojester@gmail.com>
2016-05-11 22:50:13 -0500

    Basic Layout done.

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -1,10 +1,36 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import ContactViewControl from '../contact/contact.vc'
 
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
-    context: any = {};
+    context: any = {
+        name: '',
+        num: '',
+        email: ''
+    };
+    
+    addThisContact(){
+        let person = this.context.name;
+        let phone = this.context.num;
+        let mail = this.context.email;
+        
+        if(person == undefined || person == ''){
+            alert('Type a name');
+        }
+        else{
+            console.log(person, phone, mail);
+            // console.log(person);
+            // console.log(person);
+            // this.navigator.navigate(ContactViewControl,{
+            //     // Here I am passing the input value as a parameter
+            //     parameters:{
+            //         theinput: this.context.name,
+            //     }
+            // });
+        }
+    }
 }
 
 register.viewControl('home-vc', HomeViewControl);
