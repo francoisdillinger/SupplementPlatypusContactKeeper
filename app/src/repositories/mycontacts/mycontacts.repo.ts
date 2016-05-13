f5ec6b171ff468314c6798216a39110cf19d71e3
James Jester <elmojester@gmail.com>
2016-05-13 00:47:15 -0500

    Finished with repo.

--- app/src/repositories/mycontacts/mycontacts.repo.ts
+++ app/src/repositories/mycontacts/mycontacts.repo.ts
@@ -2,25 +2,35 @@ import {async, register} from 'platypus';
 import BaseRepository from '../base/base.repo';
 
 export default class MyContactsRepository extends BaseRepository {
-             contactList: any = [];
+            //  contactList: any = [];
+            contactList: any = [];
+            newContact: any = {};
              
              createNewContact(person:any, phone:any, mail:any){
-                 let newContact = {name: person, num: phone, email: mail}
-                 let payload = this.contactList;
-                 payload.push(newContact);
-                 console.log(payload);
+                 this.newContact = {name: person, num: phone, email: mail}
+                 this.contactList.push(this.newContact);
+                 console.log(this.contactList);
              }
              getContactDetails(person:any){
                  let theContact = person;
                  for(let i = 0; i < this.contactList.length; i++){
                      let singleContact = this.contactList[i];
-                     if(singleContact.name = theContact){
-                         console.log(singleContact[i])
-                         return singleContact[i];
+                     if(singleContact.name === theContact){
+                         console.log(singleContact)
+                         return singleContact;
                      }
+                    // console.log(singleContact);
                  }
                  
              }
 }
 
 register.injectable('mycontacts-repo', MyContactsRepository);
+
+// for(var i = 0; i < contactArray.length; i++){
+//         var singleContact = contactArray[i];
+//         if(singleContact.name === theName){
+//             person = singleContact;
+//             break;
+//         }
+//     }
