7fb02bd2ae2182ad7f5808cf5465ce2110a70af8
James Jester <elmojester@gmail.com>
2016-05-13 00:46:46 -0500

    Finishing up.

--- app/src/viewcontrols/contact/contact.vc.ts
+++ app/src/viewcontrols/contact/contact.vc.ts
@@ -1,10 +1,26 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import HomeViewControl from '../home/home.vc'
+import MyContactsRepository from '../../repositories/mycontacts/mycontacts.repo';
+
 
 export default class ContactViewControl extends BaseViewControl {
     templateString: string = require('./contact.vc.html');
-
-    context: any = {};
+    constructor(private contacts: MyContactsRepository){
+        super();
+    }
+    context: any = {
+        eachContact:''
+    };
+    
+    
+    navigatedTo(params:any){
+                 let theContact = params.person;
+                 this.context.eachContact = this.contacts.getContactDetails(theContact);
+    }
+    returnHome(){
+        this.navigator.navigate(HomeViewControl);
+    }
 }
 
-register.viewControl('contact-vc', ContactViewControl);
+register.viewControl('contact-vc', ContactViewControl, [MyContactsRepository]);
