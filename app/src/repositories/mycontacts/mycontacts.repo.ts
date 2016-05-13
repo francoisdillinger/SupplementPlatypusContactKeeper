836646f322f53a952055db2c21758c09515d1303
James Jester <elmojester@gmail.com>
2016-05-13 01:03:12 -0500

    Finished commenting the repo TS file.

--- app/src/repositories/mycontacts/mycontacts.repo.ts
+++ app/src/repositories/mycontacts/mycontacts.repo.ts
@@ -2,15 +2,21 @@ import {async, register} from 'platypus';
 import BaseRepository from '../base/base.repo';
 
 export default class MyContactsRepository extends BaseRepository {
-            //  contactList: any = [];
+            // Setting initial values here
             contactList: any = [];
             newContact: any = {};
-             
+            
+            //  This function creates a new contact and pushes it to the contactList array.
+            //  I have it console logging for debugging purposes.
              createNewContact(person:any, phone:any, mail:any){
                  this.newContact = {name: person, num: phone, email: mail}
                  this.contactList.push(this.newContact);
                  console.log(this.contactList);
              }
+             
+            //  This function loops through the contactList array and finds the corresponding 
+            //  object, then sends the entire object to the contact view so the details can be 
+            //  displayed.
              getContactDetails(person:any){
                  let theContact = person;
                  for(let i = 0; i < this.contactList.length; i++){
@@ -19,18 +25,8 @@ export default class MyContactsRepository extends BaseRepository {
                          console.log(singleContact)
                          return singleContact;
                      }
-                    // console.log(singleContact);
-                 }
-                 
+                 }   
              }
 }
 
 register.injectable('mycontacts-repo', MyContactsRepository);
-
-// for(var i = 0; i < contactArray.length; i++){
-//         var singleContact = contactArray[i];
-//         if(singleContact.name === theName){
-//             person = singleContact;
-//             break;
-//         }
-//     }
